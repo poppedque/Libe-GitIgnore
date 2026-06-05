162bf0adcca2e29705481421ff1aaf67dd13f3fa
poppedque <poppedque@hotmail.com>
2026-06-04 22:12:21 -0700

    Refactor echo commands to Write-Host in PowerShell script

--- src/Windows-build/make-build.ps1
+++ src/Windows-build/make-build.ps1
@@ -1,9 +1,16 @@
-echo preview build is done
-echo ---Libe---
-echo ---Version 0.0.1---
-echo Status: Under Development
-echo build.version = 0.0.1
-echo build.date = 2026/6/4
-echo build.data = src/data
-echo builder = Poppedque
-pause
+Write-Host "preview build is done"
+Write-Host "---Libe---"
+Write-Host "---Version 0.0.1---"
+Write-Host "Status: Under Development"
+
+$buildVersion = "0.0.1"
+$buildDate = "2026/6/4"
+$buildData = "src/data"
+$builder = "Poppedque"
+
+Write-Host "build.version = $buildVersion"
+Write-Host "build.date = $buildDate"
+Write-Host "build.data = $buildData"
+Write-Host "builder = $builder"
+
+Read-Host "Press Enter to continue..."
