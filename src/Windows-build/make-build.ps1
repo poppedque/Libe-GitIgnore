7be1f214126c8925a3375d40e1436b5f31dc28a4
poppedque <poppedque@hotmail.com>
2026-06-04 22:10:02 -0700

    Create make-build.ps1

---
+++ src/Windows-build/make-build.ps1
@@ -0,0 +1,9 @@
+echo preview build is done
+echo ---Libe---
+echo ---Version 0.0.1---
+echo Status: Under Development
+echo build.version = 0.0.1
+echo build.date = 2026/6/4
+echo build.data = src/data
+echo builder = Poppedque
+pause
