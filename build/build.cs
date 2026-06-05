721ca796cb10396bd232e7ea615fa131c195e6dd
poppedque <poppedque@hotmail.com>
2026-06-04 21:10:28 -0700

    Create build.cs

---
+++ build/build.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Build
+{
+    public static void Showbuild()
+    {
+        Console.WriteLine("---Libe---");
+        Console.WriteLine("---Version 0.0.1---");
+        Console.WriteLine("Status: Under Development");
+        Console.WriteLine("build.version = 0.0.1")
+        Console.WriteLine("build.date = 2026/6/4")
+        Console.WriteLine("build.data = src/data")
+
+    }
+}
